c61969889b1d6d94e0a99489d0dabd7ec65ab17b
Efrain J Valles P <311086+efrainvalles@users.noreply.github.com>
2020-08-02 07:19:44 -0400

    Added a all things from chapter 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Products from './Products';
 import JumboTronComponent from './JumboTronComponent';
+import UserForm from './UserForm'
 
 function App() {
   return (
     <div className="App">
-      <JumboTronComponent />
+      <UserForm />
+      <JumboTronComponent body='Test for echo' />
       <Products />
     </div>
   );
